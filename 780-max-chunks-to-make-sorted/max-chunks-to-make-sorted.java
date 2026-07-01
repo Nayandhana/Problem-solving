acb34f11a1ef8044735879fe5ea23051376d20a2
Nayandhana <nayandhanagangadharan@gmail.com>
2026-07-01 09:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (97.85%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] max = new int[n];
+        int[] min = new int[n];
+
+        max[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            max[i] = Math.max(max[i-1], arr[i]);
+        }
+
+        min[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            min[i] = Math.min(min[i + 1], arr[i]);
+        }
+
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (max[i] <= min[i + 1]) res++;
+        }
+
+        return res + 1;
+    }
+}
